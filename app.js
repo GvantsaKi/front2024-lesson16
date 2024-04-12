927d6ce5f93477d25559cbb381c5db55b477bdcd
GvantsaK <gvancaa99@gmail.com>
2024-04-12 14:29:40 +0400

    add js code

---
+++ app.js
@@ -0,0 +1,29 @@
+function evenOrOdd(number) {
+	return number %2==0;
+}
+
+console.log(evenOrOdd(8));
+
+
+
+
+function rectPerimeter(a,b){
+	return(a+b)*2;
+}
+
+console.log(rectPerimeter(4,6));
+
+
+
+
+function getSquarePerimeter(a){
+	return(a*a);
+}
+
+console.log(getSquarePerimeter(4));
+
+
+
+function getRandomInt(){
+	return Math.floor(math.random()*101);
+}
